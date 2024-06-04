1beb2d0cb4c89936c1fdc17812a707e94d805f7a
Olesia <115551955+yacvals@users.noreply.github.com>
2024-06-04 21:41:48 +0300

    repair logic

--- app.py
+++ app.py
@@ -6,16 +6,19 @@ def parse_markdown(md_text):
     html_output = []
     paragraphs = md_text.split('\n\n')
 
-    bold_pattern = re.compile(r'\*\*(.*?)\*\*')
-    italic_pattern = re.compile(r'_(.*?)_')
-    monospaced_pattern = re.compile(r'`(.*?)`')
-    preformatted_pattern = re.compile(r'```(.*?)```', re.DOTALL)
+    bold_pattern = re.compile(r'(?<=\s|^)\*\*(.*?)\*\*(?=\s|$)')
+    italic_pattern = re.compile(r'(?<=\s|^)_(.*?)_(?=\s|$)')
+    monospaced_pattern = re.compile(r'(?<=\s|^)`(.*?)`(?=\s|$)')
+    preformatted_pattern = re.compile(r'^```(.*?)```$', re.DOTALL)
 
     for paragraph in paragraphs:
         if preformatted_pattern.match(paragraph):
             preformatted_text = preformatted_pattern.findall(paragraph)[0]
             html_output.append(f'<pre>{preformatted_text}</pre>')
         else:
+            if '**' in paragraph or '_' in paragraph or '`' in paragraph:
+                if paragraph.count('**') % 2 != 0 or paragraph.count('_') % 2 != 0 or paragraph.count('`') % 2 != 0:
+                    raise Exception("Invalid markdown syntax")
             paragraph = bold_pattern.sub(r'<b>\1</b>', paragraph)
             paragraph = italic_pattern.sub(r'<i>\1</i>', paragraph)
             paragraph = monospaced_pattern.sub(r'<tt>\1</tt>', paragraph)
